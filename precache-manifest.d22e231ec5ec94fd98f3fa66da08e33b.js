5d53df8dfec8e09de005edba310ceaa64942ddb0
Anup <anupsingh841208@gmail.com>
2023-06-28 01:22:13 +0530

    Updates

---
+++ precache-manifest.d22e231ec5ec94fd98f3fa66da08e33b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5076427cd2bb13205f08b7f22ea790d",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "950db68dfd69f24e4ef7",
+    "url": "/iPod/static/css/main.dfdc46de.chunk.css"
+  },
+  {
+    "revision": "a2a523a7e28050e1f113",
+    "url": "/iPod/static/js/2.b81f4071.chunk.js"
+  },
+  {
+    "revision": "e60eb46c359a399fe3462a1a90718ad7",
+    "url": "/iPod/static/js/2.b81f4071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950db68dfd69f24e4ef7",
+    "url": "/iPod/static/js/main.969a3568.chunk.js"
+  },
+  {
+    "revision": "ba269b4c261862a1e479",
+    "url": "/iPod/static/js/runtime-main.13927dba.js"
+  },
+  {
+    "revision": "f139241c251c318c7801154891072f22",
+    "url": "/iPod/static/media/background.f139241c.jpg"
+  }
+]);
